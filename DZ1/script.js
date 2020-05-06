9d5cdbf2c11f68c339b090e332ac1e025e46df4c
Sergei <vinci@bk.ru>
2020-05-06 16:58:59 +0300

    Закончил ДЗ№3

--- DZ1/script.js
+++ DZ1/script.js
@@ -1,25 +1,85 @@
-let money = prompt("Ваш бюджет на месяц", "");
+let money = +prompt("Ваш бюджет на месяц", "");
 let time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
 let appData = {
   budjet: money,
   timeData: time,
-  expenses: [],
+  expenses: {},
   optionalExpenses: {},
   income: [],
   savings: false,
 };
 
-let question1 = prompt("Введите обязательную статью расходов в этом месяце");
-let question2 = prompt("Во сколько обойдется?");
-let question3 = prompt("Введите обязательную статью расходов в этом месяце");
-let question4 = prompt("Во сколько обойдется?");
-
-appData.expenses[question1] = [question2]; //[] значит вычисляемое свойство
-appData.expenses[question3] = [question4]; // имя свойства будет взято из переменной question3
-
 //Смысл вычисляемого свойства прост: запись [question1] означает, что имя свойства необходимо взять из переменной question1.
 
-alert(appData.budjet / 30);
+for (let i = 0; i < 2; i++) {
+  let a = prompt("Введите обязательную статью расходов в этом месяце");
+  let b = prompt("Во сколько обойдется?");
 
-//Добавил
+  if (
+    typeof a === "string" &&
+    typeof a != null &&
+    typeof b != null &&
+    a != "" &&
+    b != "" &&
+    a.length < 50
+  ) {
+    appData.expenses[a] = b;
+  } else {
+    alert("Данные введены некорректно");
+  }
+}
+
+/*let i = 0;
+while(i<2){
+  let a = prompt("Введите обязательную статью расходов в этом месяце");
+  let b = prompt("Во сколько обойдется?");
+  i++;
+
+  if (
+    typeof a === "string" &&
+    typeof a != null &&
+    typeof b != null &&
+    a != "" &&
+    b != "" &&
+    a.length < 50
+  ) {
+    appData.expenses[a] = b;
+  } else {
+    alert("Данные введены некорректно");
+  }
+}
+*/
+/*
+do {
+  let a = prompt("Введите обязательную статью расходов в этом месяце");
+  let b = prompt("Во сколько обойдется?");
+  if (
+    typeof a === "string" &&
+    typeof a != null &&
+    typeof b != null &&
+    a != "" &&
+    b != "" &&
+    a.length < 50
+  ) {
+    appData.expenses[a] = b;
+  } else {
+    alert("Данные введены некорректно");
+  }
+  i++;
+} while (i < 2);
+*/
+
+appData.moneyPerDay = appData.budjet / 30;
+
+alert("Ежедневный бюджет " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+  console.log("Минмальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 200) {
+  console.log("Cредний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+  console.log("Высокий уровень достатка");
+} else {
+  console.log("Произошла ошибка");
+}
